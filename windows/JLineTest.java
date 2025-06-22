85b994a25ce98886d30d775454c0b49386903692
Werner Fouché <werner.fouche@gmail.com>
2025-06-22 17:22:40 +0200

    Code clean-up

--- windows/JLineTest.java
+++ windows/JLineTest.java
@@ -1,4 +1,5 @@
 //DEPS org.jline:jline:3.30.4
+//DEPS org.jline:jline-terminal-jansi:3.30.4
 
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
@@ -10,43 +11,50 @@ import org.jline.utils.NonBlocking;
 import java.util.concurrent.TimeUnit;
 
 void main() throws IOException, InterruptedException {
+	int EOF = -1;
+	int ENTER = 13;
+	int BACKSPACE1 = 8;
+	int BACKSPACE2 = 127;
 	int timeoutSeconds = 10; // seconds
 	Terminal terminal = TerminalBuilder.builder()
+			.jna(true)
 			.system(true)
 			.build();
 	terminal.enterRawMode();
 	var reader = terminal.reader();
 	var _reader = org.jline.utils.NonBlocking.nonBlocking("jbang", reader);
 	var writer = terminal.writer();
-	int b;
 	StringBuilder line = new StringBuilder();
+	boolean running; int b;
 	long endTimeNanos = System.nanoTime() + TimeUnit.SECONDS.toNanos(timeoutSeconds);
 	do {
 		b = _reader.peek(100);
 		if (b >= 0) {
 			b = _reader.read();
-			if (b == 8 || b == 127) {
-				writer.print('\b');
-				writer.print(' ');
-				writer.print('\b');
+			if (b == BACKSPACE1 || b == BACKSPACE2) {
 				if (line.length() > 0) {
+					writer.print('\b');
+					writer.print(' ');
+					writer.print('\b');
 					line.deleteCharAt(line.length() - 1);
 				}
 			} else {
 				writer.print((char) b);
-				if (b != 13) line.append((char) b);
+				if (b != ENTER) {
+					line.append((char) b);
+				}
 			}
 			writer.flush();
 		} else {
 			Thread.sleep(100);
 		}
-	} while (b != 13 && System.nanoTime() < endTimeNanos);
-	boolean timedOut = (System.nanoTime() >= endTimeNanos);
+		running = System.nanoTime() < endTimeNanos;
+	} while (b != EOF && b != ENTER && running);
+
+	boolean timedOut = !running;
+
 	_reader.shutdown();
-	//reader.close();
-	//writer.flush();
-	//writer.close();
-	//terminal.close();
+
 	IO.println("Timed out = " + timedOut);
 	IO.println("Line length = " + line.length());
 	IO.println("Line = " + line);
